64e732368f4b6258ef71133983410379a59e6c72
ligi <ligi@ligi.de>
2015-07-11 16:44:09 +0200

    Prevent FC when no Activity can handle  Uri
    this is the last line of defense - should be prevented before showing snack with SnackCondition
     e.g. market:// for RateSnack aka https://github.com/PomepuyN/discreet-app-rate/issues/36

--- lib/src/main/java/org/ligi/snackengage/snacks/AbstractOpenURLSnack.java
+++ lib/src/main/java/org/ligi/snackengage/snacks/AbstractOpenURLSnack.java
@@ -1,7 +1,9 @@
 package org.ligi.snackengage.snacks;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.Snackbar;
 
 public abstract class AbstractOpenURLSnack extends BaseSnack {
 
@@ -9,9 +11,13 @@ public abstract class AbstractOpenURLSnack extends BaseSnack {
 
     @Override
     public void engage() {
-        final Intent intent = new Intent(Intent.ACTION_VIEW, getUri());
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        snackContext.getAndroidContext().startActivity(intent);
+        try {
+            final Intent intent = new Intent(Intent.ACTION_VIEW, getUri());
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            snackContext.getAndroidContext().startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Snackbar.make(snackContext.getRootView(), "Activity not found :-(", Snackbar.LENGTH_LONG).show();
+        }
     }
 
 }
