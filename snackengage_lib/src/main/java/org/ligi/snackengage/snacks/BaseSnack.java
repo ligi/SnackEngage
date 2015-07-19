9f327cb897c62c40af764a5ade22e0c8bff1cf29
ligi <ligi@ligi.de>
2015-07-20 00:10:44 +0200

    Add option to add duration

--- snackengage_lib/src/main/java/org/ligi/snackengage/snacks/BaseSnack.java
+++ snackengage_lib/src/main/java/org/ligi/snackengage/snacks/BaseSnack.java
@@ -1,8 +1,11 @@
 package org.ligi.snackengage.snacks;
 
+import android.support.annotation.IntDef;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
 import android.view.View;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,6 +13,18 @@ import org.ligi.snackengage.SnackContext;
 import org.ligi.snackengage.conditions.SnackCondition;
 
 public abstract class BaseSnack implements Snack {
+
+    public static final int DURATION_INDEFINITE = -2;
+    public static final int DURATION_SHORT = -1;
+    public static final int DURATION_LONG = 0;
+
+    @IntDef({DURATION_INDEFINITE, DURATION_SHORT, DURATION_LONG})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface SnackDuration {
+    }
+
+    @SnackDuration
+    private int duration = DURATION_LONG;
 
     protected SnackContext snackContext;
     protected List<SnackCondition> conditionList = new ArrayList<>();
@@ -19,12 +34,12 @@ public abstract class BaseSnack implements Snack {
         this.snackContext = snackContext;
 
         for (final SnackCondition snackCondition : conditionList) {
-            if (!snackCondition.isAppropriate(snackContext,this)) {
+            if (!snackCondition.isAppropriate(snackContext, this)) {
                 return false;
             }
         }
 
-        Snackbar.make(snackContext.getRootView(), getText(), Snackbar.LENGTH_LONG).setAction(getActionText(), new View.OnClickListener() {
+        Snackbar.make(snackContext.getRootView(), getText(), duration).setAction(getActionText(), new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 snackContext.getStats().registerSnackClick(BaseSnack.this);
@@ -41,6 +56,11 @@ public abstract class BaseSnack implements Snack {
 
     @StringRes
     public abstract int getActionText();
+
+    public Snack withDuration(@SnackDuration int duration) {
+        this.duration = duration;
+        return this;
+    }
 
     public Snack withConditions(SnackCondition... conditions) {
         Collections.addAll(conditionList, conditions);
