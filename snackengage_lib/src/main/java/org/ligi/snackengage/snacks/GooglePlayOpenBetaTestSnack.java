2e708568928b5be3d87bf7c4f21a51c3b9c190da
ligi <ligi@ligi.de>
2015-07-31 14:46:30 +0200

    Now as there is open-betatesting on play this makes sense

---
+++ snackengage_lib/src/main/java/org/ligi/snackengage/snacks/GooglePlayOpenBetaTestSnack.java
@@ -0,0 +1,22 @@
+package org.ligi.snackengage.snacks;
+
+import android.net.Uri;
+import org.ligi.snackengage.R;
+
+public class GooglePlayOpenBetaTestSnack extends AbstractOpenURLSnack {
+
+    @Override
+    public String getText() {
+        return getString(R.string.betatest_snack_msg);
+    }
+
+    @Override
+    public String getActionText() {
+        return getString(R.string.betatest_snack_action);
+    }
+
+    @Override
+    protected Uri getUri() {
+        return Uri.parse("https://play.google.com/apps/testing/" + snackContext.getAndroidContext().getPackageName());
+    }
+}
