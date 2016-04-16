35a790c3a27311c2450861c1b869d67e219cfcf6
l3d00m <thomasvanhalen01@gmail.com>
2016-04-16 09:56:20 +0200

    extract one more string

--- snackengage_lib/src/main/java/org/ligi/snackengage/snacks/AbstractOpenURLSnack.java
+++ snackengage_lib/src/main/java/org/ligi/snackengage/snacks/AbstractOpenURLSnack.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.design.widget.Snackbar;
 
+import org.ligi.snackengage.R;
+
 public abstract class AbstractOpenURLSnack extends BaseSnack {
 
     protected abstract Uri getUri();
@@ -16,7 +18,7 @@ public abstract class AbstractOpenURLSnack extends BaseSnack {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             snackContext.getAndroidContext().startActivity(intent);
         } catch (ActivityNotFoundException e) {
-            Snackbar.make(snackContext.getRootView(), "Activity not found :-(", Snackbar.LENGTH_LONG).show();
+            Snackbar.make(snackContext.getRootView(), R.string.err_no_activity, Snackbar.LENGTH_LONG).show();
         }
     }
 
