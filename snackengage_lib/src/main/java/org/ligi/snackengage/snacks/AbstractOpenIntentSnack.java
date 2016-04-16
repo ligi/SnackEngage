1ff8288dbea2399563d0c588ba4dae672128348a
l3d00m <thomasvanhalen01@gmail.com>
2016-04-16 09:57:09 +0200

    Add a new Snack that allows custom Intents

---
+++ snackengage_lib/src/main/java/org/ligi/snackengage/snacks/AbstractOpenIntentSnack.java
@@ -0,0 +1,21 @@
+package org.ligi.snackengage.snacks;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+
+import org.ligi.snackengage.R;
+
+public abstract class AbstractOpenIntentSnack extends BaseSnack{
+
+    protected abstract Intent getIntent();
+
+    @Override
+    public void engage() {
+        try {
+            snackContext.getAndroidContext().startActivity(getIntent());
+        } catch (ActivityNotFoundException e) {
+            Snackbar.make(snackContext.getRootView(), R.string.err_no_activity, Snackbar.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ snackengage_lib/src/main/java/org/ligi/snackengage/snacks/DefaultIntentSnack.java
@@ -0,0 +1,32 @@
+package org.ligi.snackengage.snacks;
+
+import android.content.Intent;
+
+public class DefaultIntentSnack extends AbstractOpenIntentSnack {
+
+    String mActionText;
+    String mTitleText;
+    Intent mIntent;
+
+    public DefaultIntentSnack(Intent intent, String titleText, String actionText){
+        this.mIntent = intent;
+        this.mActionText = actionText;
+        this.mTitleText = titleText;
+    }
+
+
+    @Override
+    public String getActionText() {
+        return mActionText;
+    }
+
+    @Override
+    public String getText() {
+        return mTitleText;
+    }
+
+    @Override
+    protected Intent getIntent() {
+        return mIntent;
+    }
+}
