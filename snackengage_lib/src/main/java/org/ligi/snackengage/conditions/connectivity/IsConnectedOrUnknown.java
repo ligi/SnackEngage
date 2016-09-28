7bd05fa86025ddd6db2a432387da0b0b4c9af18f
Ertan Deniz <ertanden@gmail.com>
2016-09-28 20:37:37 +0300

    Fix IsConnectedOrUnkown by removing shadowed activeNetwork variable

--- snackengage_lib/src/main/java/org/ligi/snackengage/conditions/connectivity/IsConnectedOrUnknown.java
+++ snackengage_lib/src/main/java/org/ligi/snackengage/conditions/connectivity/IsConnectedOrUnknown.java
@@ -1,12 +1,9 @@
 package org.ligi.snackengage.conditions.connectivity;
 
-import android.net.NetworkInfo;
 import org.ligi.snackengage.SnackContext;
 import org.ligi.snackengage.snacks.Snack;
 
 public class IsConnectedOrUnknown extends ConnectivityAwareCondition {
-
-    protected NetworkInfo activeNetwork;
 
     @Override
     public boolean isAppropriate(final SnackContext context, final Snack snack) {
--- snackengage_lib/src/test/java/org/ligi/snackengage/TheConnectedSnackConditions.java
+++ snackengage_lib/src/test/java/org/ligi/snackengage/TheConnectedSnackConditions.java
@@ -3,8 +3,10 @@ package org.ligi.snackengage;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+
 import org.junit.Test;
 import org.ligi.snackengage.conditions.connectivity.IsConnectedOrUnknown;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -24,11 +26,19 @@ public class TheConnectedSnackConditions extends BaseTest {
         assertThat(tested.isConnectivityAware()).isTrue();
     }
 
+    @Test
+    public void shouldReturnIsAppropriateWhenPermissionGRANTED() {
+        final IsConnectedOrUnknown tested = setupSnack(PackageManager.PERMISSION_GRANTED);
+
+        assertThat(tested.isAppropriate(mockSnackContext, null)).isTrue();
+    }
+
     @NonNull
     private IsConnectedOrUnknown setupSnack(final int permissionGranted) {
         final IsConnectedOrUnknown tested = new IsConnectedOrUnknown();
 
         when(mockAndroidContext.checkCallingOrSelfPermission(Manifest.permission.ACCESS_NETWORK_STATE)).thenReturn(permissionGranted);
+        when(mockNetwork.isConnectedOrConnecting()).thenReturn(permissionGranted == PackageManager.PERMISSION_GRANTED);
 
         tested.isAppropriate(mockSnackContext, null);
         return tested;
