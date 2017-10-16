ad66111c354e4615431af7c762d7c2e1a4c51ab6
Tobias Preuss <tobias.preuss@googlemail.com>
2017-10-16 22:36:40 +0200

    Allow to customize the background color of the SnackBar.

--- snackengage-core/src/main/java/org/ligi/snackengage/snacks/BaseSnack.java
+++ snackengage-core/src/main/java/org/ligi/snackengage/snacks/BaseSnack.java
@@ -28,6 +28,7 @@ public abstract class BaseSnack implements Snack {
     private String actionText;
     private String titleText;
     private Integer actionColor = null;
+    private Integer backgroundColor = null;
 
     @Override
     public boolean opportunity(final SnackContext snackContext) {
@@ -54,6 +55,10 @@ public abstract class BaseSnack implements Snack {
 
         if (actionColor != null) {
             snackbar.setActionTextColor(actionColor);
+        }
+        if (backgroundColor != null) {
+            View snackBarView = snackbar.getView();
+            snackBarView.setBackgroundColor(backgroundColor);
         }
 
         return snackbar.setAction(actionText, new View.OnClickListener() {
@@ -106,6 +111,10 @@ public abstract class BaseSnack implements Snack {
 
     public void setActionColor(@ColorInt int color) {
         actionColor = color;
+    }
+
+    public void setBackgroundColor(@ColorInt int color) {
+        backgroundColor = color;
     }
 
     @IntDef({DURATION_INDEFINITE, DURATION_SHORT, DURATION_LONG})
