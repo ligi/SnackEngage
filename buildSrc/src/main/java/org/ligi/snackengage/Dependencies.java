fb8e8184a969d69538e09ac4402396a95851ab61
Tobias Preuss <tobias.preuss@googlemail.com>
2020-08-15 21:36:31 +0200

    Use gradle-versions-plugin v.0.29.0.

--- buildSrc/src/main/java/org/ligi/snackengage/Dependencies.java
+++ buildSrc/src/main/java/org/ligi/snackengage/Dependencies.java
@@ -18,7 +18,7 @@ public class Dependencies {
 
         public static final String ANDROID = "com.android.tools.build:gradle:3.6.3";
         public static final String MAVEN = "com.github.dcendents:android-maven-gradle-plugin:2.1";
-        public static final String VERSIONS = "com.github.ben-manes:gradle-versions-plugin:0.28.0";
+        public static final String VERSIONS = "com.github.ben-manes:gradle-versions-plugin:0.29.0";
 
     }
 
