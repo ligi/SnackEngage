bf1ae72e9bd1359ca07a7a8c7b6509b719237c28
Tobias Preuss <tobias.preuss@googlemail.com>
2021-03-02 17:23:44 +0100

    Mute Lint warning to invert method.

--- snackengage-core/src/main/java/org/ligi/snackengage/conditions/SnackCondition.java
+++ snackengage-core/src/main/java/org/ligi/snackengage/conditions/SnackCondition.java
@@ -8,6 +8,7 @@ import org.ligi.snackengage.snacks.Snack;
 
 public interface SnackCondition {
 
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     boolean isAppropriate(@NonNull SnackContext context, @NonNull final Snack snack);
 
 }
