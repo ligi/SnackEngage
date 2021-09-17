41f62b5a0ff4e7360c443635d346021a28b3579f
Tobias Preuss <tobias.preuss@googlemail.com>
2021-09-17 14:35:18 +0200

    Raise VERSION_CODE and VERSION_NAME for upcoming tag.

--- buildSrc/src/main/java/org/ligi/snackengage/Dependencies.java
+++ buildSrc/src/main/java/org/ligi/snackengage/Dependencies.java
@@ -9,8 +9,8 @@ public class Dependencies {
         public static final int MIN_SDK_VERSION = 14;
         public static final int COMPILE_SDK_VERSION = 30;
         public static final int TARGET_SDK_VERSION = 30;
-        public static final int VERSION_CODE = 28;
-        public static final String VERSION_NAME = "0.28";
+        public static final int VERSION_CODE = 29;
+        public static final String VERSION_NAME = "0.29";
 
     }
 
