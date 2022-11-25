dea542833990359a8684539d07a7a23190c8f057
Tobias Preuss <tobias.preuss@googlemail.com>
2022-11-25 22:44:07 +0100

    Replace anonymous type with lambda expression.

--- demo_app/src/main/java/org/ligi/snackengage_demo/DemoActivity.java
+++ demo_app/src/main/java/org/ligi/snackengage_demo/DemoActivity.java
@@ -27,29 +27,24 @@ public class DemoActivity extends AppCompatActivity {
         setContentView(R.layout.content);
 
         //noinspection ConstantConditions
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                SnackEngage.from(v)
-                        .withSnack(new TranslateSnack("https://www.transifex.com/projects/p/snackengage").withConditions(new IsOneOfTheseLocales(Locale.CANADA),
+        findViewById(R.id.button).setOnClickListener(view -> SnackEngage.from(view)
+                .withSnack(new TranslateSnack("https://www.transifex.com/projects/p/snackengage").withConditions(new IsOneOfTheseLocales(Locale.CANADA),
+                        new NeverAgainWhenClickedOnce(),
+                        new AfterNumberOfOpportunities(10)))
+                .withSnack(new DefaultRateSnack())
+                .withSnack(new OpenURLSnack("http://www.google.com", "URL_GOOGLE")
+                        .withConditions(new AfterNumberOfOpportunities(1),
                                 new NeverAgainWhenClickedOnce(),
-                                new AfterNumberOfOpportunities(10)))
-                        .withSnack(new DefaultRateSnack())
-                        .withSnack(new OpenURLSnack("http://www.google.com", "URL_GOOGLE")
-                                .withConditions(new AfterNumberOfOpportunities(1),
-                                        new NeverAgainWhenClickedOnce(),
-                                        new WithLimitedNumberOfTimes(3))
-                                .withDuration(BaseSnack.DURATION_LONG)
-                                .overrideActionText("Like Cookies?")
-                                .overrideTitleText("Buy'em"))
-                        .withSnack(new OpenURLSnack("http://www.bing.com", "URL_BING")
-                                .withConditions(new AfterNumberOfOpportunities(10),
-                                        new NeverAgainWhenClickedOnce())
-                                .overrideActionText("OK!"))
-                        .withSnack(new GooglePlayOpenBetaTestSnack().withConditions(new NeverAgainWhenClickedOnce(), new AfterNumberOfOpportunities(42)))
-                        .build()
-                        .engageWhenAppropriate();
-            }
-        });
+                                new WithLimitedNumberOfTimes(3))
+                        .withDuration(BaseSnack.DURATION_LONG)
+                        .overrideActionText("Like Cookies?")
+                        .overrideTitleText("Buy'em"))
+                .withSnack(new OpenURLSnack("http://www.bing.com", "URL_BING")
+                        .withConditions(new AfterNumberOfOpportunities(10),
+                                new NeverAgainWhenClickedOnce())
+                        .overrideActionText("OK!"))
+                .withSnack(new GooglePlayOpenBetaTestSnack().withConditions(new NeverAgainWhenClickedOnce(), new AfterNumberOfOpportunities(42)))
+                .build()
+                .engageWhenAppropriate());
     }
 }
