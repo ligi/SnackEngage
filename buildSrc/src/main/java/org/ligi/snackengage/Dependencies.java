315b0888ba879065164148444f6872d162fb9e08
Tobias Preuss <tobias.preuss@googlemail.com>
2026-08-13 21:47:34 +0200

    Use gradle-versions-plugin 0.54.0.

--- buildSrc/src/main/java/org/ligi/snackengage/Dependencies.java
+++ buildSrc/src/main/java/org/ligi/snackengage/Dependencies.java
@@ -16,7 +16,7 @@ public class Dependencies {
     public static class GradlePlugins {
 
         public static final String ANDROID = "com.android.tools.build:gradle:8.0.2";
-        public static final String VERSIONS = "com.github.ben-manes:gradle-versions-plugin:0.42.0";
+        public static final String VERSIONS = "com.github.ben-manes:gradle-versions-plugin:0.54.0";
 
     }
 
